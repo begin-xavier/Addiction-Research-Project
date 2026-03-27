b04a13cdc76a4adfafa79d7fcb959a7ded0ad75c
begin-xavier <1ggxavie0@gmail.com>
2026-03-27 16:25:59 -0500

    Added demographic analysis and updated load function to match

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def load_2024(filepath):
     df2024 = pd.read_csv(filepath, sep="\t", low_memory=False)
-    key_cols = ['AGE3', 'STMNMREC', 'SEDNMREC', 'HEALTH', 'SPDPSTYR', 'SEDRSSLEP', 'TRQRSSLEP', 'UDSTWDSLEEP', 'UDSVWDSLEEP']
+    key_cols = ['AGE3', 'STMNMREC', 'SEDNMREC', 'HEALTH', 'SPDPSTYR', 'SEDRSSLEP', 'TRQRSSLEP', 'UDSTWDSLEEP', 'UDSVWDSLEEP', 'IRSEX', 'NEWRACE2', 'IREDUHIGHST2', 'IRFAMIN3']
     df_filtered2024 = df2024[key_cols]
     df_filtered2024 = df_filtered2024[df_filtered2024['AGE3'].between(3, 6)]
     
@@ -10,7 +10,7 @@ def load_2024(filepath):
 
 def load_2015(filepath):
     df2015 = pd.read_csv(filepath, sep="\t", low_memory=False)
-    key_cols_2015 = ['AGE2', 'STMNMREC', 'SEDNMREC', 'HEALTH', 'SEDRSSLEP', 'TRQRSSLEP']
+    key_cols_2015 = ['AGE2', 'STMNMREC', 'SEDNMREC', 'HEALTH', 'SEDRSSLEP', 'TRQRSSLEP', 'SPDYR', 'IRSEX', 'NEWRACE2', 'IREDUHIGHST2', 'IRFAMIN3']
     df_filtered2015 = df2015[key_cols_2015]
     df_filtered2015 = df_filtered2015[df_filtered2015['AGE2'].between(5, 12)]
     
@@ -18,5 +18,5 @@ def load_2015(filepath):
 
 def load_2024_full(filepath):
     df2024 = pd.read_csv(filepath, sep="\t", low_memory=False)
-    key_cols = ['AGE3', 'STMNMREC', 'SEDNMREC', 'HEALTH', 'SPDPSTYR', 'SEDRSSLEP', 'TRQRSSLEP', 'UDSTWDSLEEP', 'UDSVWDSLEEP']
+    key_cols = ['AGE3', 'STMNMREC', 'SEDNMREC', 'HEALTH', 'SPDPSTYR', 'SEDRSSLEP', 'TRQRSSLEP', 'UDSTWDSLEEP', 'UDSVWDSLEEP', 'IRSEX', 'NEWRACE2', 'IREDUHIGHST2', 'IRFAMIN3']
     return df2024[key_cols]
